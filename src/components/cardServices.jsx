4445a79ff4f30e3d4043e9c29d559ceb78ab4bdd
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-06-04 15:50:08 -0300

    cardService.jsx Agregando traducciones a los botones de ver más

--- src/components/cardServices.jsx
+++ src/components/cardServices.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import {useState} from "react";
 
 import { Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next'
+
 export const Card = ({ title, description, image }) => {
   const [text, setText] = useState(false);
+  const [t,i18n] = useTranslation("global")
+
   return (
     <div className="flex flex-col bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-500">
     <img className="w-full md:h-64 object-cover" src={image} alt={title} />
@@ -18,7 +22,7 @@ export const Card = ({ title, description, image }) => {
           className="mt-4 md:px-32 py-2 bg-white border border-primary text-primary text-center font-bold rounded-full hover:bg-primary hover:text-white transition-colors duration-300"
         
         >
-          ver mas
+          {t("buttons.watch_more")}
         </a>
       </div>
     </div>
