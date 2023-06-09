6861f8bc9813c870fc9f305629a6fd1bff984cef
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-06-09 14:36:06 -0300

    hero.jsx Agregando validaciones para el cambio de imagenes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,18 +4,18 @@ import i18next from "i18next";
 
 const Hero = () => {
 
-    const [t] = useTranslation("global")
+    const [t,i18n] = useTranslation("global")
 
     return (
         <section className="hero md:bg-fixed bg-cover bg-no-repeat pt-[10vh] min-h-screen flex flex-col md:flex-row pb-[5vh]  ">
-           <div className=" md:col-span-5  flex items-center justify-start md:justify-center p-5 md:p-20 ">
-                <div className="flex flex-col gap-8 ">
-                    <h1 className="md:text-[7rem] text-black xs:text-[6rem] text-[5rem] font-medium tracking-wide leading-[7rem]  ">
-                    {t("hero.title")}  <span className="pr-10 text-primary">{t("hero.ideal")}</span>{i18next.language === 'es' ? null : t("hero.website")}
+           <div className=" md:col-span-5  flex items-center justify-start md:justify-center p-5 md:p-20 xs:p-0">
+                <div className="flex flex-col gap-8">
+                    <h1 className="md:text-[7rem] text-black xs:text-[6rem] text-[5rem] font-medium tracking-wide leading-[7rem] sm:text-[6rem]">
+                    {t("hero.title")} <span className="pr-10 text-primary">{t("hero.ideal")}</span>{i18next.language === 'es' ? null : t("hero.website")}
                     </h1>
                     <p className="text-white font-light text-xl tracking-wider leading-[2.5rem]">{t("hero.description")}{/* Somos future una agencia destinada a digitalizar tu Empresa, Negocio o Emprendimiento de manera sencilla  */}
                     </p>
-                    <div className="flex  ">
+                    <div className="flex">
                         <a className="bg-primary border-primary text-black text-center
                          font-semibold border-2 py-2 md:px-8 rounded-full
                          w-1/3 md:w-[25vh]"
@@ -30,7 +30,7 @@ const Hero = () => {
             
             <div className="justify-center flex flex-col md col-span-3">
 
-                <img src="https://firebasestorage.googleapis.com/v0/b/future-landing.appspot.com/o/iphoneymac.png?alt=media&token=ad5f529a-7420-4f3e-9598-416005750d5d" alt="" />
+                <img src={i18n.language === 'es' ? 'img/mockups/Future_Español.png' : 'img/mockups/Future_Inglés.png'} alt="Computadora y celular de future"/>
                 
             </div>
         </section>
